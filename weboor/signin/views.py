332d77dbdc01372cbf923846ccadd84d8c1a4fab
Raphlo <raphaellaf98@hotmail.com>
2019-04-11 15:47:53 -0400

    heroku commit

---
+++ weboor/signin/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<h1>Home Page</h1>')
+
+def news(request):
+    return HttpResponse('<h1>News</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
