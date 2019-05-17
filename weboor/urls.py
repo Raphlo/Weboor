71265f240a3e74d5026dcb1db33b7a58803de1bc
rl <raphaellaf98@hotmail.com>
2019-05-17 16:49:50 -0400

    Filled games.html + added game boxes

--- weboor/urls.py
+++ weboor/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signin/', include('signin.urls')),
+    path('Games/', include('Games.urls')),
 ]
